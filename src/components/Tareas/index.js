1bd079887cd8a34962e8cde56b136d225d719b02
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2021-08-29 15:56:01 -0700

    Se agregó listado de tareas

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as tareasActinos from './../../actions/tareasActions';
+import Spinner from './../General/Spinner';
+import Fatal from './../General/Fatal';
 
 class Tareas extends Component {
   componentDidMount(){
     this.props.traerTodas();
   }
 
-  render() {
-    console.log(this.props)
+  mostrarContenido = () => {
+    const { tareas, cargando, error } = this.props;
+
+    if(cargando){      
+      return <Spinner /> ;
+    }
+
+    if(error){      
+      return <Fatal mensaje={ error } /> ;
+    }
+
+    return Object.keys(tareas).map((usu_id) => (
+      <div key={ usu_id }>      
+        <h1>
+          Usuario { usu_id }
+        </h1>
+        <div className="contenedor_tareas">
+          { this.ponerTareas(usu_id) }
+        </div>
+      </div>
+    ));
+  }
+
+  ponerTareas = (usu_id) => {
+    const { tareas } = this.props;
+    const por_usuario = {
+      ...tareas[usu_id],
+    };
+
+    return Object.keys(por_usuario).map((tar_id) => (
+      <div key={tar_id}>
+        <input type="checkbox" defaultChecked={por_usuario[tar_id].completed} />
+        {
+          por_usuario[tar_id].title
+        }
+      </div>
+    ));
+  }
+
+  render() {    
     return (
-      <div>
-        Tareas saludar
+      <div>            
+        { this.mostrarContenido() }        
       </div>
     )
   }
